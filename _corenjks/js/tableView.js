399f6d3b9dffe05488768e29a7ab96fdd1b0086d
Christopher McCreadie <cjmccreadie@jellyfin.local>
2025-04-04 22:52:53 +0700

    added table edit flows

--- _corenjks/js/tableView.js
+++ _corenjks/js/tableView.js
@@ -1,2 +1,33 @@
-// Show the table
-document.getElementById("showBody").classList.remove("d-none");
+const url = new URL(window.location.href);
+let parts = url.pathname.split("/").filter(Boolean);
+let tableName = parts.length > 1 ? parts[parts.length - 2] : null;
+let id = url.searchParams.get("id");
+
+let whenDocumentReady = (f) => {
+  /in/.test(document.readyState)
+    ? setTimeout("whenDocumentReady(" + f + ")", 9)
+    : f();
+};
+
+whenDocumentReady(
+  (isReady = () => {
+    let getViewDone = (response) => {
+      response = JSON.parse(response);
+      if (response.data.length > 0) {
+        const data = response.data[0];
+        // Loop through the returned data and populate the form fields
+        Object.keys(data).forEach((key) => {
+          let field = document.getElementById("inp-" + key);
+          if (field) {
+            field.value = data[key]; // Set the value of the field
+          }
+        });
+      }
+      document.getElementById("showBody").classList.remove("d-none");
+    };
+    // Show the table
+    let theUrl = apiUrl + `tables/${tableName}`;
+    if (id != null) theUrl += `?id=${id}`;
+    xhrcall(1, theUrl, "", "json", "", getViewDone);
+  })
+);
